6d860bd2aae7183f3956e85f95ac9ab9a32dd7b9
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-10 09:59:40 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def dictionary(array)
+  array.collect do |key, value|
+    if value == "hello"
+      "hi"
+    elsif value == "to" || "two" || "too"
+      "2"
+    elsif value == "for" || "four"
+      "4"
+    elsif value == "be"
+      "b"
+    elsif  value == "you"
+      "u"
+    elsif value == "at"
+      "@"
+    elsif value == "and"
+      "&"
+  else value
+end
+end
+    
