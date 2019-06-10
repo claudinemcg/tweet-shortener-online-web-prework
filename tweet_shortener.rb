13eedce61844466f1da1133d62018dbf3137fc3a
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-10 10:03:01 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -21,7 +21,8 @@ def word_substituter(string)
     end 
   end.join(" ")
 end 
-word_substituter(tweet_one)
+
+
 
 def bulk_tweet_shortener(array)
   array.each do |tweet|
@@ -29,7 +30,7 @@ def bulk_tweet_shortener(array)
   end
 end
 
-bulk_tweet_shortener(tweets)
+
 
 def selective_tweet_shortener(tweet)
   if tweet.length > 140 
@@ -38,7 +39,7 @@ def selective_tweet_shortener(tweet)
     tweet 
   end 
 end 
-selective_tweet_shortener(tweet_three)
+
 
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).length > 140 
@@ -48,4 +49,4 @@ def shortened_tweet_truncator(tweet)
   end 
 end 
 
-shortened_tweet_truncator(tweet_three)
+
